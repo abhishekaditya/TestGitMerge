fc9d606b905c256cfe87928672e15c30e806b6fc
Abhishek Shukla <abhishekaditya.91@in.ibm.com>
2020-05-24 00:56:57 +0530

    changes made in CodeTest.java

---
+++ src/test/code/link/ArrayKthSmallestElement.java
@@ -0,0 +1,18 @@
+package test.code.link;
+
+import java.util.Scanner;
+
+public class ArrayKthSmallestElement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0; i<t; i++) {
+			int n = sc.nextInt();
+			
+		}
+		
+
+	}
+
+}
--- src/test/code/link/CodeTest.java
+++ src/test/code/link/CodeTest.java
@@ -5,7 +5,7 @@ public class CodeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Hello Life");
+		System.out.println("Hello Life, how are you?");
 
 	}
 
