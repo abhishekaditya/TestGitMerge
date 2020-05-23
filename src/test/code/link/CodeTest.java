f2b0bf4ca0cc625487d44aa6554b8a2efdbbc4de
Abhishek Shukla <abhishekaditya.91@in.ibm.com>
2020-05-24 00:52:31 +0530

    initial commit

---
+++ src/test/code/link/CodeTest.java
@@ -0,0 +1,12 @@
+package test.code.link;
+
+public class CodeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello Life");
+
+	}
+
+}
